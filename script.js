e5ede7d69fbd155a498ae5bfe0922cf4a37d7756
Yopixxx <edtc.torres8@ejemplo.com>
2025-07-17 22:04:59 -0500

    Update

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ function crearItem(nombre, esNuevo = false) {
   checkbox.type = "checkbox";
 
   const span = document.createElement("span");
+
+  if (esNuevo) {
+    contadorNuevos++;
+    nombre += " " + contadorNuevos;
+    li.classList.add("nuevo");
+  }
+
   span.textContent = nombre;
 
   checkbox.addEventListener("change", () => {
@@ -29,19 +36,48 @@ function crearItem(nombre, esNuevo = false) {
   li.appendChild(span);
   listaInv.appendChild(li);
 
-  if (esNuevo) {
-    contadorNuevos++;
-    actualizarContadores();
-  }
   actualizarContadores();
 }
 
 function actualizarContadores() {
-  const total = listaInv.children.length;
-  const llegadosCount = document.querySelectorAll("#listaInvitados input:checked").length;
+  const items = listaInv.children;
+  let totalIniciales = 0;
+  let pendientesIniciales = 0;
+  let llegadosCount = 0;
+
+  for (let item of items) {
+    const checkbox = item.querySelector("input[type=checkbox]");
+
+    if (checkbox.checked) {
+      llegadosCount++;
+    }
+
+    if (!item.classList.contains("nuevo")) {
+      totalIniciales++;
+      if (!checkbox.checked) {
+        pendientesIniciales++;
+      }
+    }
+  }
+
   llegados.textContent = llegadosCount;
-  pendientes.textContent = total - llegadosCount;
+  pendientes.textContent = pendientesIniciales;
   nuevos.textContent = contadorNuevos;
+
+  // Aforo
+  const aforoSpan = document.getElementById("contadorTotal");
+  aforoSpan.textContent = llegadosCount;
+  aforoSpan.classList.remove("gris", "verde", "naranja", "rojo");
+
+  if (llegadosCount < 100) {
+    aforoSpan.classList.add("gris");
+  } else if (llegadosCount < 150) {
+    aforoSpan.classList.add("verde");
+  } else if (llegadosCount < 160) {
+    aforoSpan.classList.add("naranja");
+  } else {
+    aforoSpan.classList.add("rojo");
+  }
 }
 
 // Agregar los iniciales
